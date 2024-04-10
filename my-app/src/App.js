f8e6ce94a73cca7b25bbaef5ff6821364c9b8730
Dorrety <shev1701@gmail.com>
2024-04-10 23:34:26 +0200

    create-app

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+// import './App.css';
+
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+      newTodo: '',
+    }
+  }
+  
+  render() {
+    return (
+    <div>
+      <p>HELLO</p>
+    </div> 
+    )
+
+  }
+}
+
