58a3cfcd2ed606f3b0c26308b5fb34bd9961999d
Dorrety <shev1701@gmail.com>
2024-04-11 11:28:28 +0200

    to-do-list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,14 +10,45 @@ export default class App extends React.Component {
       newTodo: '',
     }
   }
-  
+
+  handleChange = (event) => {
+    this.setState({ newTodo: event.target.value })
+    console.log(this.state)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { todos, newTodo } = this.state;
+    if (newTodo.trim() === '') return;
+    const updateTodo = [...todos, newTodo];
+    this.setState({ todos: updateTodo, newTodo: '' })
+  }
+
+  handleDelete = (index) => {
+    const { todos } = this.state;
+    const updateTodo = todos.filter((todo, todoIndex) => todoIndex !== index);
+    this.setState({ todos: updateTodo })
+  }
+
   render() {
+    const { todos, newTodo } = this.state
     return (
     <div>
-      <p>HELLO</p>
+      <h1>To-do-list</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' onChange={this.handleChange} value={newTodo} placeholder="Add new todo..." />
+          <input type='submit' value='Submit' />
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button onClick={()=> this.handleDelete(index)}>-</button>
+            </li>
+          ))}
+        </ul>
     </div> 
     )
-
   }
 }
 
